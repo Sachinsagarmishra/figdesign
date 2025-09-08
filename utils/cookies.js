0ef098000a6ab2d760418fa236fd5a6c6981b591
Sachinsagarmishra <80114340+Sachinsagarmishra@users.noreply.github.com>
2025-09-08 10:54:50 +0530

    Update cookies.js

--- utils/cookies.js
+++ utils/cookies.js
@@ -1,10 +1,12 @@
 import Cookies from 'js-cookie'
 
-export const saveHistory = (data) => {
-  Cookies.set('shortHistory', JSON.stringify(data))
+export function getHistory() {
+  const saved = Cookies.get('history')
+  return saved ? JSON.parse(saved) : []
 }
 
-export const loadHistory = () => {
-  const saved = Cookies.get('shortHistory')
-  return saved ? JSON.parse(saved) : []
+export function saveHistory(item) {
+  const history = getHistory()
+  const newHistory = [item, ...history]
+  Cookies.set('history', JSON.stringify(newHistory))
 }
